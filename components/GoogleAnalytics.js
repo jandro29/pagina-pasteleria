eda9dd98b9c38d7153f3add717926ee06a865601
jandro29 <ascolanr@gmail.com>
2025-10-20 11:26:35 -0500

    .

---
+++ components/GoogleAnalytics.js
@@ -0,0 +1,47 @@
+'use client';
+
+import { usePathname, useSearchParams } from 'next/navigation';
+import Script from 'next/script';
+import { useEffect } from 'react';
+
+export default function GoogleAnalytics() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const gaId = process.env.NEXT_PUBLIC_GA_ID;
+
+  useEffect(() => {
+    if (gaId && typeof window !== 'undefined' && window.gtag) {
+      const url = pathname + (searchParams.toString() ? `?${searchParams.toString()}` : '');
+      window.gtag('config', gaId, {
+        page_path: url,
+      });
+    }
+  }, [pathname, searchParams, gaId]);
+
+  if (!gaId) {
+    return null;
+  }
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+      />
+      <Script
+        id="google-analytics-config"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gaId}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    </>
+  );
+}
