142a02172885ff1d1684b496e5dfe830d3795515
jandro29 <ascolanr@gmail.com>
2025-10-20 11:30:30 -0500

    .

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -7,7 +7,7 @@ import { useEffect } from 'react';
 export default function GoogleAnalytics() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const gaId = process.env.NEXT_PUBLIC_GA_ID;
+  const gaId = "G-EN6FNMQQKY";
 
   useEffect(() => {
     if (gaId && typeof window !== 'undefined' && window.gtag) {
@@ -17,10 +17,6 @@ export default function GoogleAnalytics() {
       });
     }
   }, [pathname, searchParams, gaId]);
-
-  if (!gaId) {
-    return null;
-  }
 
   return (
     <>
